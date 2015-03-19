4f4cc0e75203c17a804d97c4964cb11bedc105fb
Terry Yin <terry@odd-e.com>
2015-03-19 21:54:33 +0800

    absolution

--- app/mailers/newsletter/news_mailer.rb
+++ app/mailers/newsletter/news_mailer.rb
@@ -3,14 +3,14 @@ class Newsletter::NewsMailer < ActionMailer::Base
 
   def last_mail_with_tag(tag, user, default = nil)
     last_mail = Newsletter::Newsletter.tagged_with(tag).last || default
-    raise "No newsletter with the tag '#{tag}#, please add it." if not last_mail
+    raise "No newsletter with the tag '#{tag}, please add it." if not last_mail
     news_mail(last_mail, user)
   end
 
   def news_mail(newsletter, user)
     @newsletter = newsletter
     @contactable = user
-    mail(to: user.email, subject: render(partial:"extend", locals:{body:@newsletter.subject}).strip, template_name:"news_mail")
+    mail(to: user.email, subject: render(partial:"extend", locals:{body:@newsletter.subject}).strip, template_path:"newsletter/news_mailer", template_name:"news_mail")
   end
 
 end
