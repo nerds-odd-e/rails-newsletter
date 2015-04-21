91d9c8010d7b193d25b7cfad5fba5496f7aa5fe3
Terry Yin <terry@odd-e.com>
2015-04-21 17:53:06 +0800

    huge

--- app/controllers/newsletter/mail_templates_controller.rb
+++ app/controllers/newsletter/mail_templates_controller.rb
@@ -48,14 +48,6 @@ class Newsletter::MailTemplatesController < ApplicationController
     respond_with(@mail_template)
   end
 
-  def self.method_missing(method, *args, &block)
-    if (method.to_s.end_with?('_path') || method.to_s.end_with?('_url')) && main_app.respond_to?(method)
-      main_app.send(method, *args)
-    else
-      super
-    end
-  end
-
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_newsletter
